3185c6811a4531df38e8d5393265f4841c797f39
Marco Munizaga <marco@marcopolo.io>
2019-03-08 23:51:18 +0100

    Add more transformations

--- src/transformers/__tests__/flowToTs.ts
+++ src/transformers/__tests__/flowToTs.ts
@@ -1,4 +1,11 @@
-import { transformImports } from "../maybeTypes";
+import {
+  transformImports,
+  transformFunctionTypes,
+  transformIdentifiers,
+  transformTypeAliases,
+  transformInterfaces,
+  transformTypeCastings
+} from "../flowToTs";
 import * as JSCodeShift from "JSCodeShift";
 jest.autoMockOff();
 
@@ -29,6 +36,198 @@ describe("Transform Import statements", () => {
     const collection = j(input);
 
     transformImports(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+});
+
+describe("Functions", () => {
+  it("Transforms function types", () => {
+    const input = "type F = (A, B) => C";
+    const out = "type F = (a: A, b: B) => C;";
+    const collection = j(input);
+
+    transformFunctionTypes(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Preserves arg names", () => {
+    const input = "type F = (coolArg: A, B) => C";
+    const out = "type F = (coolArg: A, b: B) => C;";
+    const collection = j(input);
+
+    transformFunctionTypes(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Works with generics", () => {
+    const input = "type F = (A<number>, B) => C";
+    const out = "type F = (a: A<number>, b: B) => C;";
+    const collection = j(input);
+
+    transformFunctionTypes(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+});
+
+describe("Exact types", () => {
+  it("Transforms exact types", () => {
+    const input = "type A = {| foo: number |}";
+    const out = "type A = {\n  foo: number\n};";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Transforms exact types with type params", () => {
+    const input = "type A<B> = {| foo: number, bar: B |}";
+    const out = "type A<B> = {\n  foo: number,\n  bar: B\n};";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Transforms exact types with type params and constraints", () => {
+    const input = "type A<B: number> = {| foo: number, bar: B |}";
+    const out = "type A<B extends number> = {\n  foo: number,\n  bar: B\n};";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Transforms mixed", () => {
+    const input = "type A = {foo: mixed}";
+    const out = "type A = {\n  foo: unknown\n};";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Handles spreads", () => {
+    const input = "type A = {...B, c: number}";
+    const out = "type A = {\n  c: number\n} & B;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Transforms Indexer", () => {
+    const input = "type A<K> = {[K]: number}";
+    const out = "type A<K> = {\n  [k: K]: number\n};";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Transforms function types", () => {
+    const input = "type A = () => string";
+    const out = "type A = () => string;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+});
+
+describe("Handles opaque types", () => {
+  it("Handles simple opaque", () => {
+    const input = "opaque type A = string";
+    const out = "type A = string;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+});
+
+describe("Handles interface definition and readonly", () => {
+  it("Handles interface with variance", () => {
+    const input = "interface A { +b: B, -c: C }";
+    const out = "interface A {\n  readonly b: B;\n  c: C;\n}";
+    const collection = j(input);
+
+    transformInterfaces(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+});
+
+describe("Transforms castings", () => {
+  it("Can handle a simple casting", () => {
+    const input = "1 + (a: number)";
+    const out = "1 + (a as number)";
+    const collection = j(input);
+
+    transformTypeCastings(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+});
+
+describe("Can handle common special Flow Types", () => {
+  it("Can handle $Exact", () => {
+    const input = "type B = $Exact<A>";
+    const out = "type B = A;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Can handle $Keys", () => {
+    const input = "type B = $Keys<A>";
+    const out = "type B = keyof A;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Can handle $Values", () => {
+    const input = "type B = $Values<A>";
+    const out = "type B = A[keyof A];";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Can handle $Call", () => {
+    const input = "type B = $Call<A>";
+    const out = "type B = ReturnType<A>;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Can handle $ElementType", () => {
+    const input = "type B = $ElementType<T, k>";
+    const out = "type B = T[k];";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Can handle $PropertyType", () => {
+    const input = "type B = $PropertyType<T, 'foo'>";
+    const out = 'type B = T["foo"];';
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
+    expect(collection.toSource()).toEqual(out);
+  });
+
+  it("Can handle $Diff", () => {
+    const input = "type B = $Diff<A, B>";
+    const out = "type B = Exclude<A, B>;";
+    const collection = j(input);
+
+    transformTypeAliases(collection, j);
     expect(collection.toSource()).toEqual(out);
   });
 });
--- src/transformers/flowToTs.ts
+++ src/transformers/flowToTs.ts
@@ -11,75 +11,221 @@ import {
   JSCodeshift,
   TSType,
   TSTypeAnnotation,
-  Identifier
+  Identifier,
+  FunctionTypeParam,
+  TSTypeParameter,
+  TypeParameter,
+  TSPropertySignature,
+  TSIndexSignature,
+  TypeAlias,
+  OpaqueType,
+  ObjectTypeProperty,
+  ObjectTypeIndexer,
+  ObjectTypeSpreadProperty
 } from "jscodeshift";
 import { Node } from "ast-types/gen/nodes";
 import { FlowTypeKind, TSTypeKind } from "ast-types/gen/kinds";
 import { Collection } from "jscodeshift/src/Collection";
+import { NodePath } from "recast";
 
-function convertToTSType(
-  jts: JSCodeshift,
-  type: FlowTypeKind
-): TSTypeKind | null {
+const lowerCaseFirst = (s: string) => s.replace(/^(.)/, m => m.toLowerCase());
+
+function convertPropertiesToTsProperties(
+  j: JSCodeshift,
+  p: ObjectTypeProperty | ObjectTypeIndexer
+): TSPropertySignature | TSIndexSignature {
+  if (p.type == "ObjectTypeProperty") {
+    const tsType = convertToTSType(j, p.value);
+    if (tsType) {
+      const typeAnnotation = j.tsTypeAnnotation(tsType);
+
+      const key = j.identifier((p.key as Identifier).name);
+      return j.tsPropertySignature.from({
+        key,
+        typeAnnotation,
+        readonly: !!p.variance && (p.variance as any).kind === "plus"
+      });
+    }
+  } else {
+    let name: string;
+    if (p.id) {
+      name = p.id.name;
+    } else if (
+      p.key.type === "GenericTypeAnnotation" &&
+      p.key.id &&
+      p.key.id.type === "Identifier"
+    ) {
+      name = lowerCaseFirst(p.key.id.name);
+    } else if (p.key.type === "NumberTypeAnnotation") {
+      name = "n";
+    } else {
+      name = "key";
+    }
+    const typeAnnotation = j.tsTypeAnnotation(convertToTSType(j, p.key));
+    const value = j.tsTypeAnnotation(convertToTSType(j, p.value));
+
+    return j.tsIndexSignature.from({
+      parameters: [j.identifier.from({ name, typeAnnotation })],
+      typeAnnotation: value
+    });
+  }
+}
+
+function convertToTSType(j: JSCodeshift, type: FlowTypeKind): TSTypeKind {
   switch (type.type) {
     case "StringTypeAnnotation":
-      return jts.tsStringKeyword();
+      return j.tsStringKeyword();
     case "NumberTypeAnnotation":
-      return jts.tsNumberKeyword();
+      return j.tsNumberKeyword();
     case "NumberLiteralTypeAnnotation":
-      return jts.tsLiteralType(jts.numericLiteral(type.value));
+      return j.tsLiteralType(j.numericLiteral(type.value));
     case "StringLiteralTypeAnnotation":
-      return jts.tsLiteralType(jts.stringLiteral(type.value));
+      return j.tsLiteralType(j.stringLiteral(type.value));
     case "BooleanLiteralTypeAnnotation":
-      return jts.tsLiteralType(jts.booleanLiteral(type.value));
+      return j.tsLiteralType(j.booleanLiteral(type.value));
     case "MixedTypeAnnotation":
-      return jts.tsUnknownKeyword();
+      return j.tsUnknownKeyword();
     case "NullLiteralTypeAnnotation":
     case "VoidTypeAnnotation":
       // return jts.tsUnionType([jts.tsNullKeyword(), jts.tsVoidKeyword()]);
-      return jts.tsNullKeyword();
+      return j.tsNullKeyword();
     case "NullableTypeAnnotation":
-      let innerType = convertToTSType(jts, type.typeAnnotation);
-      return jts.tsUnionType([innerType, jts.tsNullKeyword()]);
+      let innerType = convertToTSType(j, type.typeAnnotation);
+      return j.tsUnionType([innerType, j.tsNullKeyword()]);
     case "GenericTypeAnnotation":
       let id = type.id;
+      if (id.type === "Identifier") {
+        // Handle special cases
+        switch (id.name) {
+          case "$Exact":
+            return convertToTSType(j, type.typeParameters.params[0]);
+          case "$Keys": {
+            let t = convertToTSType(j, type.typeParameters.params[0]);
+            return j.tsTypeOperator.from({
+              typeAnnotation: t,
+              operator: "keyof"
+            });
+          }
+          case "$PropertyType":
+          case "$ElementType": {
+            let obj = convertToTSType(j, type.typeParameters.params[0]);
+            let index = convertToTSType(j, type.typeParameters.params[1]);
+            return j.tsIndexedAccessType.from({
+              indexType: index,
+              objectType: obj
+            });
+          }
+          case "$REST":
+          case "$Diff": {
+            let T = convertToTSType(j, type.typeParameters.params[0]);
+            let U = convertToTSType(j, type.typeParameters.params[1]);
+            return j.tsTypeReference.from({
+              typeName: j.identifier("Exclude"),
+              typeParameters: j.tsTypeParameterInstantiation.from({
+                params: [T, U]
+              })
+            });
+          }
+          case "$Values": {
+            let t = convertToTSType(j, type.typeParameters.params[0]);
+            let tsKeyOf = j.tsTypeOperator.from({
+              typeAnnotation: t,
+              operator: "keyof"
+            });
+            return j.tsIndexedAccessType.from({
+              indexType: tsKeyOf,
+              objectType: t
+            });
+          }
+          case "$Call": {
+            const t = convertToTSType(j, type.typeParameters.params[0]);
+            return j.tsTypeReference.from({
+              typeName: j.identifier("ReturnType"),
+              typeParameters: j.tsTypeParameterInstantiation.from({
+                params: [t]
+              })
+            });
+          }
+        }
+      }
       let typeArgs = type.typeParameters;
       let tsTypeArgs = null;
       if (typeArgs && typeArgs.params.length) {
         tsTypeArgs = typeArgs.params
-          .map(t => convertToTSType(jts, t))
+          .map(t => convertToTSType(j, t))
           .filter(t => !!t);
       }
       if (id.type === "Identifier") {
-        return jts.tsTypeReference(
-          jts.identifier(id.name),
-          tsTypeArgs ? jts.tsTypeParameterInstantiation(tsTypeArgs) : null
+        return j.tsTypeReference(
+          j.identifier(id.name),
+          tsTypeArgs ? j.tsTypeParameterInstantiation(tsTypeArgs) : null
         );
       }
 
-      // TODO
-      return null;
+      throw Error("Unhandled Identifier type");
     case "ObjectTypeAnnotation":
-      let tsMembers = type.properties.map(p => {
-        if (p.type == "ObjectTypeProperty") {
-          let tsType = convertToTSType(jts, p.value);
-          if (tsType) {
-            let tsTypeAnnotation = jts.tsTypeAnnotation(tsType);
+      const spreads = type.properties
+        .filter(p => p.type === "ObjectTypeSpreadProperty")
+        .map((p: ObjectTypeSpreadProperty) => convertToTSType(j, p.argument));
 
-            let identifier = jts.identifier((p.key as Identifier).name);
-            let propertySig = jts.tsPropertySignature(identifier);
-            propertySig.typeAnnotation = tsTypeAnnotation;
-            return propertySig;
-          }
-        }
+      const tsMembers: Array<TSPropertySignature | TSIndexSignature> = [];
+      tsMembers.push(
+        ...type.properties
+          .filter(p => p.type === "ObjectTypeProperty")
+          .map(p => convertPropertiesToTsProperties(j, p as ObjectTypeProperty))
+      );
 
-        // TODO handle spread case
-        return null;
-      });
+      tsMembers.push(
+        ...type.indexers.map(i => convertPropertiesToTsProperties(j, i))
+      );
 
-      return jts.tsTypeLiteral(tsMembers);
+      const tsType = j.tsTypeLiteral(tsMembers);
+      if (spreads.length) {
+        return j.tsIntersectionType([tsType, ...spreads]);
+      }
+
+      return tsType;
+    case "FunctionTypeAnnotation":
+      const tsParams = type.params.map(t => convertFunctionTypeParam(j, t));
+      const tsReturn = j.tsTypeAnnotation(convertToTSType(j, type.returnType));
+      const tsFn = j.tsFunctionType(tsParams);
+      tsFn.typeAnnotation = tsReturn;
+      return tsFn;
   }
-  return null;
+  throw new Error(`Unhandled case for ${type.type}`);
+}
+
+function convertTypeParameter(
+  j: JSCodeshift,
+  parameter: TypeParameter
+): TSTypeParameter {
+  return j.tsTypeParameter(
+    parameter.name,
+    parameter.bound && convertToTSType(j, parameter.bound.typeAnnotation)
+  );
+}
+
+function convertFunctionTypeParam(
+  j: JSCodeshift,
+  typeParam: FunctionTypeParam
+): Identifier {
+  let argCounter = 0;
+  const optional = typeParam.optional;
+  const typeAnnotation = j.tsTypeAnnotation(
+    convertToTSType(j, typeParam.typeAnnotation)
+  );
+
+  const paramName = typeParam.name
+    ? typeParam.name.name
+    : typeParam.typeAnnotation.type === "GenericTypeAnnotation" &&
+      typeParam.typeAnnotation.id.type === "Identifier"
+    ? lowerCaseFirst(typeParam.typeAnnotation.id.name)
+    : `arg${argCounter++}`;
+
+  let tsParam = j.identifier(paramName);
+  tsParam.typeAnnotation = typeAnnotation;
+  tsParam.optional = optional;
+  return tsParam;
 }
 
 // Mutates collection
@@ -117,6 +263,90 @@ export function transformImports(
     if (node.importKind === "type") {
       path.parentPath.node.importKind = null;
     }
+  });
+}
+
+export function transformFunctionTypes(
+  collection: Collection<any>,
+  j: JSCodeshift,
+  options?: Options
+) {
+  collection.find(j.FunctionTypeAnnotation).forEach(path => {
+    j(path).replaceWith(convertToTSType(j, path.node));
+  });
+}
+
+export function transformTypeAliases(
+  collection: Collection<any>,
+  j: JSCodeshift,
+  options?: Options
+) {
+  const visitor = (
+    path: NodePath<TypeAlias | OpaqueType, TypeAlias | OpaqueType>
+  ) => {
+    const node = path.node;
+    const flowType = node.type === "TypeAlias" ? node.right : node.impltype;
+    const tsTypeAnnotation = convertToTSType(j, flowType);
+    const tsTypeParams = node.typeParameters
+      ? node.typeParameters.params.map(param => convertTypeParameter(j, param))
+      : null;
+
+    const typeAlias = j.tsTypeAliasDeclaration.from({
+      id: node.id,
+      typeAnnotation: tsTypeAnnotation,
+      typeParameters: tsTypeParams && j.tsTypeParameterDeclaration(tsTypeParams)
+    });
+
+    j(path).replaceWith(typeAlias);
+  };
+
+  collection.find(j.TypeAlias).forEach(visitor);
+  collection.find(j.OpaqueType).forEach(visitor);
+}
+
+export function transformInterfaces(
+  collection: Collection<any>,
+  j: JSCodeshift,
+  options?: Options
+) {
+  collection.find(j.InterfaceDeclaration).forEach(path => {
+    const body = j.tsInterfaceBody(
+      path.node.body.properties.map(p =>
+        convertPropertiesToTsProperties(j, p as ObjectTypeProperty)
+      )
+    );
+    const id = path.node.id;
+    const typeParameters = path.node.typeParameters
+      ? j.tsTypeParameterDeclaration(
+          path.node.typeParameters.params.map(p => convertTypeParameter(j, p))
+        )
+      : null;
+
+    const tsInterface = j.tsInterfaceDeclaration.from({
+      id,
+      typeParameters,
+      body
+    });
+
+    j(path).replaceWith(tsInterface);
+  });
+}
+
+export function transformTypeCastings(
+  collection: Collection<any>,
+  j: JSCodeshift,
+  options?: Options
+) {
+  collection.find(j.TypeCastExpression).forEach(path => {
+    const asExp = j.tsAsExpression.from({
+      expression: path.node.expression,
+      typeAnnotation: convertToTSType(
+        j,
+        path.node.typeAnnotation.typeAnnotation
+      )
+    });
+
+    j(path).replaceWith(asExp);
   });
 }
 
